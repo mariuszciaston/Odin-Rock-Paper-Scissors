b6f7e624f6c265aed54656c486b992d4a97a899d
Mariusz <casmar90@gmail.com>
2022-10-21 18:44:48 +0200

    Buttons somehow work, working on it

--- game.js
+++ game.js
@@ -1,43 +1,61 @@
 // Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
 
-
-
-
-
 // Info Board  --------------------------------------------------
 
 console.log('Let\'s play Rock, Paper, Scissors!');
 console.log("Choose: Rock, Paper or Scissors");
-
-const info = document.querySelector('#info');
-
-const content1 = document.createElement('div');
-content1.classList.add('play');
-content1.textContent = 'Let\'s play Rock, Paper, Scissors!';
-info.appendChild(content1);
+// const info = document.querySelector('#info');
+// const content1 = document.createElement('div');
+// content1.classList.add('play');
+// content1.textContent = 'Let\'s play Rock, Paper, Scissors!';
+// info.appendChild(content1);
 
 // Player -------------------------------------------------
+let playerScore = 0;
+let computerScore = 0;
+let playerTotal = 0;
+let computerTotal = 0;
+
 // const rock = document.querySelector('#rock');
 // const paper = document.querySelector('#paper');
 // const scissors = document.querySelector('#scissors');
-
-
-//  function playerPlay() {
-
-
-
-
-
 
 rock.addEventListener('click', () => {
     playerSelection = 'Rock';
     console.log('Your selection: ' + playerSelection);
     console.log('Computer selection: ' + computerSelection);
     playRound(playerSelection, computerSelection);
+    console.log ('Round score: ' + [playerScore + ':' + computerScore]);
 
     playerTotal += playerScore;
     computerTotal += computerScore;
     console.log('Total score: ' + [playerTotal + ':' + computerTotal]);
+
+    if (playerTotal == 5  && computerTotal < 5) {
+        console.log('You win, congratulations!');
+    }
+
+    else if (playerTotal == 5 && computerTotal == 5) {
+        console.log('Match draw.');
+    } 
+
+    else if (playerTotal < 5  && computerTotal == 5){
+        console.log('You lose, Skynet is coming!');
+    }
+
+
+    if (playerTotal == 5 || computerTotal == 5) {
+        playAgain = window.confirm('Do you want to play again?')
+    
+        playerScore = 0;
+        computerScore = 0;
+        playerTotal = 0;
+        computerTotal = 0;
+
+        if (playAgain == true){
+            console.log("Choose: Rock, Paper or Scissors");  
+        }
+    }
 
 });
 
@@ -46,11 +64,24 @@ paper.addEventListener('click', () => {
     console.log('Your selection: ' + playerSelection);
     console.log('Computer selection: ' + computerSelection);
     playRound(playerSelection, computerSelection);
+    console.log ('Round score: ' + [playerScore + ':' + computerScore]);
 
     playerTotal += playerScore;
     computerTotal += computerScore;
     console.log('Total score: ' + [playerTotal + ':' + computerTotal]);
 
+    if (playerTotal == 5 || computerTotal == 5) {
+        playAgain = window.confirm('Do you want to play again?')
+    
+        playerScore = 0;
+        computerScore = 0;
+        playerTotal = 0;
+        computerTotal = 0;
+
+        if (playAgain == true){
+            console.log("Choose: Rock, Paper or Scissors");  
+        }
+    }
 
 });
 
@@ -59,16 +90,26 @@ scissors.addEventListener('click', () => {
     console.log('Your selection: ' + playerSelection);
     console.log('Computer selection: ' + computerSelection);
     playRound(playerSelection, computerSelection);
+    console.log ('Round score: ' + [playerScore + ':' + computerScore]);
 
     playerTotal += playerScore;
     computerTotal += computerScore;
     console.log('Total score: ' + [playerTotal + ':' + computerTotal]);
 
+    if (playerTotal == 5 || computerTotal == 5) {
+        playAgain = window.confirm('Do you want to play again?')
+    
+        playerScore = 0;
+        computerScore = 0;
+        playerTotal = 0;
+        computerTotal = 0;
 
+        if (playAgain == true){
+            console.log("Choose: Rock, Paper or Scissors");  
+        }
+    }
 });
 
-
-//  }
 
 
 // Computer -----------------------------------------------
@@ -81,11 +122,7 @@ function computerPlay() {
 let computerSelection = computerPlay();
 
 
-
 // Round --------------------------------------------------
-
-
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         playerScore = 1;
@@ -126,68 +163,4 @@ function playRound(playerSelection, computerSelection) {
         console.log('Something went wrong.'); // Shows when user input is incorrect (rock, ROCK, RocK) ...  Can be deleted after playerSelection parameter is made case-insensitive
         // alert('Something went wrong.');
     }
-    return ('Round score: ' + [playerScore + ':' + computerScore]);
 }
-
-
-
-
-
-
-
-// Game ---------------------------------------------------
-
-let playerScore = 0;
-let computerScore = 0;
-
-let playerTotal = 0;
-let computerTotal = 0;
-
-
-//     if (playerTotal > computerTotal) {
-//         console.log('You win, congratulations!');
-//     } else if (playerTotal === computerTotal) {
-//         console.log('Match draw.');
-//     } else {
-//         console.log('You lose, Skynet is coming!');
-//     }
-
-
-// game();
-
-// function game() {
-
-//         playerPlay();
-//         const computerSelection = computerPlay();
-//         // console.log('Computer selection: ' + computerSelection);
-
-//         // const currentRound = playRound(playerSelection, computerSelection);
-//         // console.log(currentRound);
-
-//         playerTotal += playerScore;
-//         computerTotal += computerScore;
-//     }
-
-//     console.log('Total score: ' + [playerTotal + ':' + computerTotal]);
-
-//     if (playerTotal > computerTotal) {
-//         console.log('You win, congratulations!');
-//     } else if (playerTotal === computerTotal) {
-//         console.log('Match draw.');
-//     } else {
-//         console.log('You lose, Skynet is coming!');
-//     }
-
-// playAgain = window.confirm('Do you want to play again?');
-
-
-
-
-// const playerSelection = undefined;
-
-//  if (playerSelection != undefined) {
-//     console.log('okej');
-//  }
-
-// while (playerSelection == undefined) {
-// }
